3dbd63e28773472e53bedeeee6b48b66f46b859c
Arthur-Reif <rreifarthur@gmail.com>
2026-09-23 16:28:44 -0300

    implementada categoria na AppDbContext

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -5,8 +5,25 @@ namespace DeskFlowAPI
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Chamado> Chamados { get; set; }
         public DbSet<Interacao> Interacoes { get; set; }
-        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Categoria>(categoria =>
+            {
+                categoria.ToTable("Tb_categoria");
+                
+                categoria.HasKey(c => c.Id);
+                categoria.Property(c => c.Nome)
+                .HasColumnName("nomeCategoria")
+                .HasMaxLength(200)
+                .IsRequired();
+                
+            });
+        }
     }
 }
