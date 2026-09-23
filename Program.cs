eb97ff2f368d40b3364b7fb43c53624e51b2bf2c
Arthur-Reif <rreifarthur@gmail.com>
2026-09-23 17:20:17 -0300

    Mudança na program efetuada, adicionado o builder do AppDbContext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using DeskFlowAPI;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 if (app.Environment.IsDevelopment())
 {
